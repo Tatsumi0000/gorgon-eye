6ee61c39b0b83249c501d2d4d85e5413b032f6a9
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-12-29 01:58:20 +0900

    cobra-cli init

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import "fmt"
+import "github.com/Tatsumi0000/gorgon-eye/cmd"
 
 func main() {
-	fmt.Println("Hello, World!")
+	cmd.Execute()
 }
